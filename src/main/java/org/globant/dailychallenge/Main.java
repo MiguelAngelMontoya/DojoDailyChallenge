aa4f68075928ee2acf561b9d2f9da58a95330560
MiguelAngelMontoya <miguelangel.montoya@globant.com>
2023-09-08 04:05:38 -0600

    Update: Add Pascal triangle solution

--- src/main/java/org/globant/dailychallenge/Main.java
+++ src/main/java/org/globant/dailychallenge/Main.java
@@ -1,10 +1,14 @@
 package org.globant.dailychallenge;
 
 import org.globant.dailychallenge.ministack.Escenarios;
+import org.globant.dailychallenge.pascaltriangle.PascalTriangle;
 
 public class Main {
     public static void main(String[] args) {
         Escenarios escenarios = new Escenarios();
         escenarios.probarEscenario();
+
+        PascalTriangle pascalTriangle = new PascalTriangle();
+        pascalTriangle.generateTriangle(5);
     }
 }
